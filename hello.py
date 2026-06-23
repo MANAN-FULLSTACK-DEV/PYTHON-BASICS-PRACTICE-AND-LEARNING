431e1e273dbe728948cf09d1c3ae168b3c0be858
MANAN <sk8200998@gmail.com>
2026-06-24 01:44:15 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,1 @@
-print("what kind of food you want to order sir :")
-a=input("food itmes:\nmomos\npizza\ncholebhature\nsamosa " )
-while (a=='food items'):
-    print(a)
-c=input()
-print("ok",c, "will be at your home in some minutes sir")
+print("new begining with github")
