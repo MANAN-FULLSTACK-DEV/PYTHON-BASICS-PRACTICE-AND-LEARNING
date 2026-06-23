a60baa82d6515224072da1f386f6268b5314fa93
MANAN <sk8200998@gmail.com>
2026-06-24 01:41:06 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,6 @@
+print("what kind of food you want to order sir :")
+a=input("food itmes:\nmomos\npizza\ncholebhature\nsamosa " )
+while (a=='food items'):
+    print(a)
+c=input()
+print("ok",c, "will be at your home in some minutes sir")
